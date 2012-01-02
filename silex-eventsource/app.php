358b2aab9cd62ab7b6a7837f6918516e30eae60d
Igor Wiedler <igor@wiedler.ch>
2012-01-02 23:06:16 +0100

    remove useless response from use statement

--- silex-eventsource/app.php
+++ silex-eventsource/app.php
@@ -23,7 +23,7 @@ $app->get('/', function () {
 $app->get('/stream', function () use ($app) {
     $headers = Stream::getHeaders();
 
-    $stream = function () use ($app, &$response) {
+    $stream = function () use ($app) {
         set_time_limit(0);
         ini_set('memory_limit', '512M');
 
