174dd88dc00ddae8bd9f29f74b4af5e061285bfd
Igor Wiedler <igor@wiedler.ch>
2012-01-22 12:16:16 +0100

    use pub/sub and bind in dispatch.js, makes much more sense

--- websockets/dispatch.js
+++ websockets/dispatch.js
@@ -1,8 +1,9 @@
 var io = require('socket.io').listen(81),
     zmq = require('zmq'),
-    sock = zmq.socket('pull');
+    sock = zmq.socket('sub');
 
-sock.connect('tcp://127.0.0.1:5555');
+sock.subscribe('');
+sock.bind('tcp://*:5555');
 
 sock.on('message', function (msg) {
     var event = JSON.parse(msg);
--- websockets/trigger.php
+++ websockets/trigger.php
@@ -2,8 +2,8 @@
 
 $context = new ZMQContext();
 
-$sock = $context->getSocket(ZMQ::SOCKET_PUSH);
-$sock->bind("tcp://*:5555");
+$sock = $context->getSocket(ZMQ::SOCKET_PUB);
+$sock->connect("tcp://127.0.0.1:5555");
 
 $msg = json_encode(array('type' => 'debug', 'data' => array('foo', 'bar', 'baz')));
 $sock->send($msg);
