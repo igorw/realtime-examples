e31dc79b4efdd172de93d9cce720316efed16cc8
Igor Wiedler <igor@wiedler.ch>
2012-01-22 13:29:06 +0100

    normalize quotes

--- websockets/trigger.php
+++ websockets/trigger.php
@@ -3,7 +3,7 @@
 $context = new ZMQContext();
 
 $sock = $context->getSocket(ZMQ::SOCKET_PUB);
-$sock->connect("tcp://127.0.0.1:5555");
+$sock->connect('tcp://127.0.0.1:5555');
 
 $msg = json_encode(array('type' => 'debug', 'data' => array('foo', 'bar', 'baz')));
 $sock->send($msg);
